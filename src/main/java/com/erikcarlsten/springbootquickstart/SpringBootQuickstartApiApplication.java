437270e79eb45b17c5cf65671869a85442a8c091
Erik S Carlsten <ecarlste@gmail.com>
2017-02-15 18:47:03 -0600

    Initial commit

---
+++ src/main/java/com/erikcarlsten/springbootquickstart/SpringBootQuickstartApiApplication.java
@@ -0,0 +1,12 @@
+package com.erikcarlsten.springbootquickstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootQuickstartApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootQuickstartApiApplication.class, args);
+	}
+}
