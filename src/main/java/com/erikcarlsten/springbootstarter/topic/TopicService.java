1f0c41257548e9c5a3fd9e343684779cf955ff65
Erik S Carlsten <ecarlste@gmail.com>
2017-02-16 00:06:02 -0600

    Removed leftover hard coded topics array

--- src/main/java/com/erikcarlsten/springbootstarter/topic/TopicService.java
+++ src/main/java/com/erikcarlsten/springbootstarter/topic/TopicService.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @Service
@@ -12,18 +11,12 @@ public class TopicService {
 
     private final TopicRepository topicRepository;
 
-    private List<Topic> topics = new ArrayList<>(Arrays.asList(
-            new Topic("spring", "Spring Framework", "Spring Framework Description"),
-            new Topic("java", "Core Java", "Core Java Description"),
-            new Topic("javascript", "JavaScript", "JavaScript Description")
-    ));
-
     @Autowired
     public TopicService(TopicRepository topicRepository) {
         this.topicRepository = topicRepository;
     }
 
-    public List<Topic> getAllTopics() {
+    List<Topic> getAllTopics() {
         List<Topic> topics = new ArrayList<>();
 
         topicRepository.findAll()
@@ -32,19 +25,19 @@ public class TopicService {
         return topics;
     }
 
-    public Topic getTopic(String id) {
+    Topic getTopic(String id) {
         return topicRepository.findOne(id);
     }
 
-    public void addTopic(Topic topic) {
+    void addTopic(Topic topic) {
         topicRepository.save(topic);
     }
 
-    public void updateTopic(Topic topic) {
+    void updateTopic(Topic topic) {
         topicRepository.save(topic);
     }
 
-    public void deleteTopic(String id) {
+    void deleteTopic(String id) {
         topicRepository.delete(id);
     }
 }
